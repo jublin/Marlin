6e432e4de3bfb6a84deed88316636d12e7f46209
Justin Moore <jublin@users.noreply.github.com>
2022-11-04 09:28:57 -0700

    Change Name

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -135,7 +135,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "FrankenVinci Pro 1.0"
+#define CUSTOM_MACHINE_NAME "Da Vinci Pro 1.0 SKR3EZ"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
