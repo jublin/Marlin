c629266ccb01d16334212eb77aa8a9b2a18cc9f6
Justin Moore <jublin@users.noreply.github.com>
2022-11-23 10:14:56 -0800

    fix offsets, pins

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1476,7 +1476,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -38.5, -6.5, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -41.15, -6.69, -1.20 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
